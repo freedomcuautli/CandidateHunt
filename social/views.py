e265f5c028a79d0d254671c5f91ae411dff8c53c
freedomcuautli <erik.alvarado.pl@gmail.com>
2016-07-21 21:22:25 -0700

    added index fix

--- social/views.py
+++ social/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render_to_response
 
-from blog.models import posts
+from social.models import posts
 
 def home(request):
 	return render_to_response('index.html')
