db7eed835f95a67508d9afbc265cc7f3cd1caa07
freedomcuautli <erik.alvarado.pl@gmail.com>
2016-07-21 21:42:57 -0700

    added theme

--- snappypunch/settings.py
+++ snappypunch/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    './social/template/assets/',
+    '/home/snappypunch/Documents/snappypunch/snappypunch/social/templates/assets/',
 ]
 
 # Static files (CSS, JavaScript, Images)
