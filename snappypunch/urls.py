d7129daca5f722642fc812a847c7414cf84c5dc6
freedomcuautli <erik.alvarado.pl@gmail.com>
2016-07-21 21:19:09 -0700

    added index fix

--- snappypunch/urls.py
+++ snappypunch/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls.defaults import patterns,include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns['',
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'snappypunch.social.views.home',name='home'),
+    url(r'^$', 'social.views.home',name='home'),
 ]
